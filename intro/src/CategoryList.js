81b8b42ebf34919ad06e411817f5ca1143d4d3c1
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-12-02 00:24:57 +0300

    Atakan Demir
    State & Map

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem, } from 'reactstrap'
 
 export default class CategoryList extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { categories: [{ categoryId: 1, categoryName: "Beverages" }, { categoryId: 2, categoryName: "Condiments" }, { categoryId: 3, categoryName: "Foods" }] }
+    // }
+    state = { categories: [{ categoryId: 1, categoryName: "Beverages" }, { categoryId: 2, categoryName: "Condiments" }, { categoryId: 3, categoryName: "Foods" }] }
 
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
+
                 <ListGroup>
-                    <ListGroupItem>Category 1</ListGroupItem>
-                    <ListGroupItem>Category 2</ListGroupItem>
-                    <ListGroupItem>Category 3</ListGroupItem>
-                    <ListGroupItem>Category 4</ListGroupItem>
-                    <ListGroupItem>Category 5</ListGroupItem>
+                    {
+                        this.state.categories.map(category => (
+                            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+                        ))
+                    }
                 </ListGroup>
             </div>
         )
