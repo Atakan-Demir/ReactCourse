3a359fd9e9977f427aaeb326b7ccaa067dee647f
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-12-04 19:03:32 +0300

    Atakan Demir
    Reducer ve Store

---
+++ redux-intro/src/redux/reducers/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import reducers from "./index";
+
+
+
+const configureStore = () => {
+    return createStore(reducers);
+}
+
+
+export default configureStore;
