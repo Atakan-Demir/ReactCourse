7f9523b5c2b84d23c4dbf0d365be61bafd961760
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2024-12-11 14:26:44 +0300

    Atakan Demir

--- northwind-redux/src/redux/reducers/index.js
+++ northwind-redux/src/redux/reducers/index.js
@@ -3,12 +3,14 @@ import changeCategoryReducer from './changeCategoryReducer';
 import categoryListReducer from './categoryListReducer';
 import productListReducer from './productListReducer';
 import cartReducer from './cartReducer';
+import saveProductReducer from './saveProductReducer';
 
 const rootReducer = combineReducers({
     changeCategoryReducer, // changeCategoryReducer:changeCategoryReducer (key ve value aynıysa birini yazmak yeterli)
     categoryListReducer,
     productListReducer,
-    cartReducer
+    cartReducer,
+    saveProductReducer
 });
 
 export default rootReducer;
